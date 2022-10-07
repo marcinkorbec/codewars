fa5eba9d1290c3a1d8a1f4746d125557dd5e5457
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-10-07 23:37:59 +0200

    add

---
+++ suma tablicy/test.js
@@ -0,0 +1,12 @@
+const {assert} = require("chai");
+
+it("example tests", ()=>{
+  assert.strictEqual( sumArray(null)                     , 0 );
+  assert.strictEqual( sumArray([ ])                      , 0 );
+  assert.strictEqual( sumArray([ 3 ])                    , 0 );
+  assert.strictEqual( sumArray([ 3, 5 ])                 , 0 );
+  assert.strictEqual( sumArray([ 6, 2, 1, 8, 10 ])       , 16 );
+  assert.strictEqual( sumArray([ 0, 1, 6, 10, 10 ])      , 17 );
+  assert.strictEqual( sumArray([ -6, -20, -1, -10, -12 ]), -28 );
+  assert.strictEqual( sumArray([ -6, 20, -1, 10, -12 ])  , 3 );
+});
