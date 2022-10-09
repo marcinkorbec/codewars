400b47b9382ac8ec96018120e39f5f1818a4a4f9
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-10-10 00:32:53 +0200

    add

--- Friend or Foe/index.js
+++ Friend or Foe/index.js
@@ -7,3 +7,35 @@ function friend(friends){
     })
     return array;
   }
+
+
+//rozwiazania innych
+function friend(friends){
+    return friends.filter(n => n.length === 4)
+}
+
+const friend = friends => friends.filter(friend => friend.length == 4);
+
+function friend(friends){
+  
+    //Create new array called "myFriends" for add your friends
+    var i,
+        len = friends.length,
+        myFriends = [];
+    
+    
+    for (i = 0; i < len; i++) {
+          
+         //Check for names with length equal to four and it is not a number
+         if(friends[i].length == 4 && isNaN(friends[i])) {
+            myFriends.push(friends[i]);
+         }
+        
+    }
+      
+    return myFriends;
+  }
+
+
+  const friend = f => f.filter(n => (isNaN(n) && n.length==4))
+
